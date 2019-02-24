ceea0a74139d0fa8b6b7f2ba7fac6304c004e2d7
masha-b <mashabudryte@gmail.com>
2019-02-24 21:38:22 +0000

    5.7 Showing Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @articles = Article.new(article_params)
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
   end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
